48519ca0c6dfb4928db1cfb1dbf1940291e07830
Yuma Matsui <oshino.iiyo@gmail.com>
2023-09-25 10:50:26 +0900

    no1 solved

--- no1/no1.go
+++ no1/no1.go
@@ -1,8 +1,10 @@
 // 変数のシャドーイング
+// -> 内側のブロック内で変数名を再宣言した際に発生
 
 package no1
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -18,8 +20,10 @@ func createDefaultClient() (http.Client, error) {
 }
 
 // NG例
-func No1() {
+func NG() {
 	var client http.Client
+
+	// ブロック内で変数をシャドーイングしてしまっている
 	if tracing {
 		client, err := createClientWithTracing()
 		if err != nil {
@@ -33,4 +37,43 @@ func No1() {
 		}
 		log.Println(client)
 	}
+}
+
+func SolutionA() {
+	var client http.Client
+
+	// 一時変数の活用
+	if tracing {
+		c, err := createClientWithTracing()
+		if err != nil {
+			log.Fatal(err)
+		}
+		client = c
+		log.Panicln(client)
+	} else {
+		c, err := createDefaultClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+		client = c
+		log.Println(client)
+	}
+}
+
+func SolutionB() {
+	// client, errを変数宣言して直接代入
+	var client http.Client
+	var err error
+
+	if tracing {
+		client, err = createClientWithTracing()
+	} else {
+		client, err = createDefaultClient()
+	}
+
+	// client, errに関する処理を共通化可能
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(client)
 }
